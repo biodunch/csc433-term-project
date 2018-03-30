cbf58dce70af06180e5ccf75bf22918f4d7fab39
Emmanuel Kehinde <emmakoko1996@gmail.com>
2018-03-30 12:49:08 +0100

    Fix Gradle iss

--- app/src/main/java/com/emmanuelkehinde/stream_app/ui/login/LoginRepository.kt
+++ app/src/main/java/com/emmanuelkehinde/stream_app/ui/login/LoginRepository.kt
@@ -1,4 +1,9 @@
 package com.emmanuelkehinde.stream_app.ui.login
 
 class LoginRepository {
+
+
+    private fun loginUser(email: String, password: String, successHandler: Unit, failureHandler: Unit) {
+
+    }
 }
