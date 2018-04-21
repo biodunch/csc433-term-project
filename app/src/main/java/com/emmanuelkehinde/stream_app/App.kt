538d18c89c14ec4905774d88945b141ca2a5e4d8
Kehinde Emmanuel <emmakoko1996@gmail.com>
2018-04-21 12:09:24 +0100

    Complete streaming using Camera2 API

--- app/src/main/java/com/emmanuelkehinde/stream_app/App.kt
+++ app/src/main/java/com/emmanuelkehinde/stream_app/App.kt
@@ -48,7 +48,7 @@ class App: Application() {
             // You should not init your app in this process.
             return
         }
-        LeakCanary.install(this)
+//        LeakCanary.install(this)
     }
 
 
